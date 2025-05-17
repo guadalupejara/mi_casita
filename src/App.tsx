b66723d2d50d05634097704ffd886ccd6557be64
lupe0614 <lupejara0614@gmail.com>
2025-05-16 19:54:11 -0700

    established routes and created home page - blank

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home'
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Let's start Lupe - this is landing page
-        </a>
-      </header>
+       <div>
+      <Routes>
+        <Route>
+        <Route path="/" element={<Home />} />
+        </Route>
+
+      </Routes>
+      </div>
     </div>
+   </BrowserRouter>
   );
 }
 
