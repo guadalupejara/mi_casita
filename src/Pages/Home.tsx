7932010acf06553d75c0466d7115c21f32566a30
lupe0614 <lupejara0614@gmail.com>
2025-05-29 16:40:09 -0700

    created skeleton for main pages and button component

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import NavButton from '../components/NavButton'
 function Home(){
 
 return(
@@ -6,8 +7,11 @@ return(
     <React.Fragment>
 <main className='min-h-screen'>
     <div>
-        
         <h1>I am home page</h1>
+    </div>
+    <div>
+    <NavButton label="Go to Login" to="/login" />
+    <NavButton label="Go to Register" to="/register" />
     </div>
 </main></React.Fragment>
 )
