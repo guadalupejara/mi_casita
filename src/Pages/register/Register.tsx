d4da9f10917f13bd8881fb30dbd01b6f1af0310c
lupe0614 <lupejara0614@gmail.com>
2025-06-27 17:26:32 -0700

    created registration page , installed formik, yup, lucide

--- src/Pages/register/Register.tsx
+++ src/Pages/register/Register.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from 'react';
+import TransparentCard from './transparentCard';
+import RegisterForm from './registerForm';
+import { Link } from 'react-router-dom';
 
-function Register(){
-    return(
+function Register() {
+    return (
         <React.Fragment>
-            <main>
-                <div className='min-h-screen'>
-                    <h1>I am Register</h1>
-                </div>
-            </main>
+        <main className="h-screen w-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/door2.jpg')" }}>
+            <div className="flex items-center justify-center h-full w-full bg-black bg-opacity-50">
+               <TransparentCard>
+                <h1 className="text-black text-4xl font-bold flex justify-center">Register</h1>
+                <p className='text-grey-100 mb-6 flex justify-center'>Already a user? {' '}
+                    <Link to="/login" className="text-orange-900 hover:underline ml-1">
+                    Sign in
+                    </Link>
+                </p>
+                <RegisterForm/>
+           </TransparentCard>
+            </div>
+        </main>
         </React.Fragment>
-    )
+    );
 }
 
-export default Register
+export default Register;
---
+++ src/Schema/validationSchema.ts
@@ -0,0 +1,14 @@
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+  firstName: yup.string().required('First name is required'),
+  lastName: yup.string().required('Last name is required'),
+  email: yup.string().email('Invalid email address').required('Email is required'),
+  password: yup.string().min(8, 'Password must be at least 8 characters').required('Password is required'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password')], 'Passwords must match')
+    .required('Please confirm your password'),
+});
+
+export default formSchema;
