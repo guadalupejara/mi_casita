b840a086a82a4320061e18d457a3a420d1363598
lupe0614 <lupejara0614@gmail.com>
2025-08-17 20:20:05 -0700

    read me images added

---
+++ src/pages/stickyNotes.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import StickyNotesComponent from "../components/stickyNotes/stickyNotes";
+
+function StickyNotes(){
+    return(
+        <React.Fragment>
+<div>Welcome to Sticky Notes</div>
+<StickyNotesComponent />
+        </React.Fragment>
+    )
+}
+
+export default StickyNotes
